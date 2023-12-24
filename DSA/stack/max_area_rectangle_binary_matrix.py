45407633b61a5449334457b0c0346ec5ec0e8c10
prakashatul1 <prakashatul1@gmail.com>
2023-12-25 01:17:42 +0530

    stack 3

--- DSA/stack/max_area_rectangle_binary_matrix.py
+++ DSA/stack/max_area_rectangle_binary_matrix.py
@@ -1,20 +1,17 @@
 from maximum_area_histogram import find_maximum_area_histogram
 
 
-def find_max_area_rectangle_binary_matrix(a):
-    mx = find_maximum_area_histogram(a[0])
-    v = a[0]
-    for i in range(1, len(a)):
-        for j in range(len(a[i])):
-            if a[i][j] == 0:
+def find_max_area_rectangle_binary_matrix(a_list):
+    mx = find_maximum_area_histogram(a_list[0])
+    v = a_list[0]
+    for i in range(1, len(a_list)):
+        for j in range(len(a_list[i])):
+            if a_list[i][j] == 0:
                 v[j] = 0
             else:
-                v[j] = (a[i][j] + v[j])
+                v[j] = (a_list[i][j] + v[j])
         # print(v)
         mx = max(mx, find_maximum_area_histogram(v))
-
-
-        # mx = max(mx, find_maximum_area_histogram([x + y if x > 1 else 0 for x, y in zip(a[i], a[i - 1])]))
 
     return mx
 
