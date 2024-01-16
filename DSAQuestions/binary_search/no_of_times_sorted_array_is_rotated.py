23d12e6c5db6d20673d7b2732962c91aab68c3da
prakashatul1 <prakashatul1@gmail.com>
2024-01-16 14:29:38 +0530

    binary search

---
+++ DSAQuestions/binary_search/no_of_times_sorted_array_is_rotated.py
@@ -0,0 +1,23 @@
+def find_no_of_times(arr):
+
+    start = 0
+    end = len(arr) - 1
+
+    while start <= end:
+
+        mid = (start + end) // 2
+        nexte = (mid + 1) % len(arr)
+        previous = (mid + len(arr) - 1) % len(arr)
+
+        if arr[mid] <= arr[nexte] and arr[mid] <= arr[previous]:
+            return mid
+        elif arr[start] <= arr[mid]:
+            start = mid + 1
+        elif arr[end] <= arr[end]:
+            end = mid - 1
+
+    return -1
+
+
+
+
