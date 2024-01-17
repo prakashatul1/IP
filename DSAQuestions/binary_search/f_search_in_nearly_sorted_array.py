517e12e051225ae00e9e793c0a4080cebc580ff1
prakashatul1 <prakashatul1@gmail.com>
2024-01-18 04:10:50 +0530

    binary search error fix

---
+++ DSAQuestions/binary_search/f_search_in_nearly_sorted_array.py
@@ -0,0 +1,21 @@
+def search_in_nearly_sorted_array(arr, ele):
+    start = 0
+    end = len(arr) - 1
+
+    while start <= end:
+
+        mid = start + ((end - start) // 2)
+        if ele == arr[mid]:
+            return mid
+        elif mid >= start and arr[mid - 1] == ele:
+            return mid - 1
+        elif mid <= end and arr[mid + 1] == ele:
+            return mid + 1
+        elif ele < arr[mid]:
+            end = mid - 2
+        elif ele > arr[mid]:
+            start = mid + 2
+
+
+array1 = [5, 6, 7, 8, 10 , 9]
+print(search_in_nearly_sorted_array(array1, 5))
