887575c25b3c3079f307b664a8902e1e7907783a
prakashatul1 <prakashatul1@gmail.com>
2024-01-22 10:47:20 +0530

    variable length sliding window

---
+++ DSAQuestions/a_sliding_window/variable size sliding window/b_minimum_subarray_size_with_sum_k.py
@@ -0,0 +1,37 @@
+def minSubArrayLen(self, target: int, nums: list[int]) -> int:
+
+    i, j = 0, 0
+    length = len(nums)
+    summ = 0
+    mini = None
+
+    while j < length:
+
+        summ = summ + nums[j]
+
+        if summ >= target:
+
+            if not mini or mini > j - i + 1:
+                mini = j - i + 1
+
+        if mini:
+
+            while j - i + 1 > mini or summ > target:
+
+                summ = summ - nums[i]
+                i += 1
+
+                if summ >= target:
+
+                    if mini > j - i + 1:
+                        mini = j - i + 1
+
+        j += 1
+
+    return mini if mini else 0
+
+
+
+print(minSubArrayLen())
+
+
