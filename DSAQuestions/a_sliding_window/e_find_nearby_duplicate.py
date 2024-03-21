80425e7057b45681667adbab95c7a05876e488c5
prakashatul1 <prakashatul1@gmail.com>
2024-03-22 02:12:38 +0530

    sliding window

--- DSAQuestions/a_sliding_window/e_find_nearby_duplicate.py
+++ DSAQuestions/a_sliding_window/e_find_nearby_duplicate.py
@@ -3,18 +3,13 @@ def containsNearbyDuplicate(nums, k):
     window = set()
 
     while j < len(nums):
-
-        if j - i + 1 > k:
+        if j - i > k:
             window.remove(nums[i])
             i += 1
-
         if nums[j] in window:
             return True
-
         window.add(nums[j])
-
         j += 1
-
     return False
 
 
@@ -23,4 +18,4 @@ arr2, k2 = [1, 0, 1, 1], 1  # result True
 arr3, k3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 9], 3  # result True
 arr4, k4 = [0, 1, 2, 3, 2, 5], 3  # result True
 
-print(containsNearbyDuplicate(arr3, k3))
+print(containsNearbyDuplicate(arr1, k1))
