f02c65aa7c351a3c468f0a037af5eafe3f01d436
prakashatul1 <prakashatul1@gmail.com>
2024-04-04 10:18:47 +0530

    updated logest substring with no repeating character

--- DSAQuestions/a_sliding_window/variable size sliding window/c_longest_substring_with_no_repeating_char.py
+++ DSAQuestions/a_sliding_window/variable size sliding window/c_longest_substring_with_no_repeating_char.py
@@ -1,35 +1,58 @@
-def find_longest_substring(string_alpha: str) -> int:
+def find_longest_substring(s: str) -> int:
+    # i, j = 0, 0
+    # length = len(string_alpha)
+    # cal_dict = {}
+    # calculation_unique = 0
+    #
+    # while j < length:
+    #
+    #     # calculation w.r.t j
+    #     if string_alpha[j] not in cal_dict:
+    #         cal_dict[string_alpha[j]] = 1
+    #     else:
+    #         cal_dict[string_alpha[j]] += 1
+    #
+    #     # getting one of many result
+    #     # checking if last loop result is more close
+    #     if cal_dict[string_alpha[j]] == 1:
+    #         calculation_unique = max((j - i + 1), calculation_unique)
+    #
+    #     # adjustment for next window
+    #
+    #     while cal_dict[string_alpha[j]] > 1:
+    #
+    #         if cal_dict[string_alpha[i]] == 1:
+    #             del cal_dict[string_alpha[i]]
+    #         else:
+    #             cal_dict[string_alpha[i]] -= 1
+    #         i += 1
+    #
+    #     j += 1
+    #
+    # return calculation_unique
+    if len(s) == 1:
+        return 1
+    elif len(s) == 0:
+        return 0
+
     i, j = 0, 0
-    length = len(string_alpha)
-    cal_dict = {}
-    calculation_unique = 0
+    a = ""
+    max_len = 0
 
-    while j < length:
+    while j < len(s):
 
-        # calculation w.r.t j
-        if string_alpha[j] not in cal_dict:
-            cal_dict[string_alpha[j]] = 1
+        if not s[j] in a:
+            a += s[j]
+            max_len = max(max_len, len(a))
         else:
-            cal_dict[string_alpha[j]] += 1
-
-        # getting one of many result
-        # checking if last loop result is more close
-        if cal_dict[string_alpha[j]] == 1:
-            calculation_unique = max((j - i + 1), calculation_unique)
-
-        # adjustment for next window
-
-        while cal_dict[string_alpha[j]] > 1:
-
-            if cal_dict[string_alpha[i]] == 1:
-                del cal_dict[string_alpha[i]]
-            else:
-                cal_dict[string_alpha[i]] -= 1
-            i += 1
+            while s[j] in a:
+                i += 1
+                a = a[1:]
+            a += s[j]
 
         j += 1
 
-    return calculation_unique
+    return max_len
 
 
 string1 = "abbcde"
---
+++ HLD/i_google_docs/google_docs.md
@@ -0,0 +1,25 @@
+concurrent edit
+------------------------
+- line by line
+- event (operational transformation)
+- diff (differential sync)
+- 
+
+- event user had different internet connection
+- hence different latency
+
+event (operational transformation)
+-------------
+keep log of information in terms of events
+
+operations
+- insert
+- delete
+- update
+- return
+
+
+
+diff (differential sync)
+-------------------
+ 
