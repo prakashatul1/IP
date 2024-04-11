01f91f78a5288b07e79ae1013fd26fa266e72ba4
prakashatul1 <prakashatul1@gmail.com>
2024-04-11 11:51:37 +0530

    product of array except self

--- DSAQuestions/a_array_and_hashing/c_product_of_array_except_self.py
+++ DSAQuestions/a_array_and_hashing/c_product_of_array_except_self.py
@@ -1,3 +1,16 @@
+# https://leetcode.com/problems/product-of-array-except-self/
+"""
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+Example 2:
+
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+"""
+
+
 def productExceptSelf(nums):
     res = [1] * (len(nums))
 
