7c1455767372689eb82b11662b6ec7e5b0245e79
prakashatul1 <prakashatul1@gmail.com>
2024-05-29 13:42:37 +0530

    factory pattern

--- DSAQuestions/b_linked_list/e_lru_cache.py
+++ DSAQuestions/b_linked_list/e_lru_cache.py
@@ -1,82 +1,106 @@
+
 class Node:
 
-    def __init__(self, key, val):
-        self.val = val
+    def __init__(self, key, val) -> None:
         self.key = key
+        self.val = val
         self.prev = None
         self.next = None
 
-    def __repr__(self):
-        return f"{self.key},{self.val}"
+    def __repr__(self) -> str:
+        return f"{self.val}"
 
 
-class LRUCache:
+class LRU:
 
-    def __init__(self, capacity: int):
-        self.capacity = capacity
+    def __init__(self, capacity) -> None:
+        self.capacity = capacity if capacity > 0 else 0
         self.cache = {}
-
-        # left LRU, right most recent
         self.left, self.right = Node(0, 0), Node(0, 0)
         self.left.next, self.right.prev = self.right, self.left
 
-    # from left
     def remove(self, node):
         prev, nxt = node.prev, node.next
         prev.next, nxt.prev = nxt, prev
 
-    # from right
     def insert(self, node):
         prev, nxt = self.right.prev, self.right
         prev.next = nxt.prev = node
         node.next, node.prev = nxt, prev
 
-    def get(self, key: int) -> int:
+    def get(self, key):
+
         if key in self.cache:
             self.remove(self.cache[key])
             self.insert(self.cache[key])
-            print(self.cache)
             return self.cache[key].val
+        return None
 
-        return -1
+    def put(self, key, value):
 
-    def put(self, key: int, value: int) -> None:
+        if self.capacity:
 
-        if key in self.cache:
-            self.remove(self.cache[key])
+            if key in self.cache:
+                self.remove(self.cache[key])
 
-        self.cache[key] = Node(key, value)
-        self.insert(self.cache[key])
+            self.cache[key] = Node(key, value)
+            self.insert(self.cache[key])
 
-        print(self.right.prev)
+            if len(self.cache) > self.capacity:
+                lru = self.left.next
+                self.remove(lru)
 
-        if len(self.cache) > self.capacity:
-            lru = self.left.next
-            self.remove(lru)
-            del self.cache[lru.key]
-
-        print(self.cache)
+                del self.cache[lru.key]
 
 
-lru = LRUCache(3)
+# l1 = LRU(3)
+# l1.put("one", "one")
+# assert l1.get("one") == "one"
+# l1.put("two", "two")
+# assert l1.get("two") == "two"
+# l1.put("three", "three")
+# assert l1.get("three") == "three"
+# l1.put("four", "four")
+# assert l1.get("four") == "four"
+# assert l1.get("one") == None
 
-# Test inserting elements
-lru.put(1, 1)  # cache: {1}
-lru.put(2, 2)  # cache: {1, 2}
-lru.put(3, 3)  # cache: {1, 2, 3}
-print(lru.get(1))  # access 1, cache should now be {2, 3, 1}
-lru.put(4, 4)  # evicts key 2, cache should now be {3, 1, 4}
+l1 = LRU(3)
+l1.put("one", "one")
+assert l1.get("one") == "one"
+l1.put("two", "two")
+assert l1.get("two") == "two"
+l1.put("three", "three")
+l1.get("one")
+l1.put("four", "four")
+print(l1.cache)
 
-# Verify that the least recently used key '2' was evicted
-assert lru.get(2) == -1, "Key 2 should be evicted; expected -1"
+# l1 = LRU(3)
+# l1.put("one", "one")
+# l1.put("two", "two")
+# l1.put("three", "three")
+# l1.put("four", "four")
+# print(l1.cache)
 
-# Access key 1 to make it the most recently used
-assert lru.get(1) == 1, "Key 1 should be available; expected 1"
-lru.put(5, 5)  # evicts key 3, cache should now be {1, 4, 5}
+# l1 = LRU(3)
+# l1.put("one", "one")
+# l1.put("two", "two")
+# l1.put("three", "three")
+# l1.put("four", "four")
+# l1.put("one", "1")
+# print(l1.cache)
 
-# Verify new inserts and evictions
-assert lru.get(3) == -1, "Key 3 should be evicted; expected -1"
-assert lru.get(4) == 4, "Key 4 should be available; expected 4"
-assert lru.get(5) == 5, "Key 5 should be available; expected 5"
+# l1 = LRU(3)
+# l1.put("one", "one")
+# l1.put("two", "two")
+# l1.put("three", "three")
+# l1.put("one", "1")
+# l1.put("seven", "seven")
+# print(l1.cache)
 
-print("All tests passed!")
+
+# l1 = LRU(0)
+# l1.put("one", "one")
+# print(l1.get("one"))
+# print(l1.cache)
+
+
--- DesignPattern/Creational/factory.py
+++ DesignPattern/Creational/factory.py
@@ -1,27 +1,59 @@
-class Burger:
-    def __init__(self, ingredients):
-        self.ingredients = ingredients
-
-    def print(self):
-        print(self.ingredients)
+from abc import ABC, abstractmethod
 
 
-class BurgerFactory:
-
-    def createCheeseBurger(self):
-        ingredients = ["bun", "cheese", "beef-patty"]
-        return Burger(ingredients)
-
-    def createDeluxeCheeseBurger(self):
-        ingredients = ["bun", "tomatoe", "lettuce", "cheese", "beef-patty"]
-        return Burger(ingredients)
-
-    def createVeganBurger(self):
-        ingredients = ["bun", "special-sauce", "veggie-patty"]
-        return Burger(ingredients)
+class Vehicle(ABC):
+    @abstractmethod
+    def getType(self) -> str:
+        pass
 
 
-burgerFactory = BurgerFactory()
-burgerFactory.createCheeseBurger().print()
-burgerFactory.createDeluxeCheeseBurger().print()
-burgerFactory.createVeganBurger().print()
+class Car(Vehicle):
+    def getType(self) -> str:
+        return "Car"
+
+
+class Bike(Vehicle):
+    def getType(self) -> str:
+        return "Bike"
+
+
+class Truck(Vehicle):
+    def getType(self) -> str:
+        return "Truck"
+
+
+class VehicleFactory(ABC):
+    @abstractmethod
+    def createVehicle(self) -> Vehicle:
+        pass
+
+
+class CarFactory(VehicleFactory):
+    # Write your code here
+    def createVehicle(self) -> Vehicle:
+        return Car()
+
+
+class BikeFactory(VehicleFactory):
+    # Write your code here
+    def createVehicle(self) -> Vehicle:
+        return Bike()
+
+
+class TruckFactory(VehicleFactory):
+    # Write your code here
+    def createVehicle(self) -> Vehicle:
+        return Truck()
+
+
+carFactory = CarFactory()
+truckFactory = TruckFactory()
+bikeFactory = BikeFactory()
+
+myCar = carFactory.createVehicle()
+myTruck = truckFactory.createVehicle()
+myBike = bikeFactory.createVehicle()
+
+print(myCar.getType())  # "Car"
+print(myTruck.getType())  # "Truck"
+print(myBike.getType())  # "Bike"
